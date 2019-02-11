4ec191d110fdd08c96f2e62a475dbb416ce306b7
Marco DiPlacido <marcodiplacido@Marcos-MacBook-Pro.local>
2019-02-10 21:18:53 -0500

    adding dots loader

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import LoadingDots from './loading-dots';
 
 const Header = () => {
     return (
@@ -9,6 +10,7 @@ const Header = () => {
             <NavLink to="/courses" activeClassName="active">Courses</NavLink>
             {" | "}
             <NavLink to="/about" activeClassName="active">About</NavLink>
+            <LoadingDots dots={20} interval={100} />
         </nav>
     )
 }
---
+++ src/components/common/loading-dots.tsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+export interface LoadingDotsProps {
+    interval: number;
+    dots: number;
+}
+
+export interface LoadingDotsState {
+    frame: number;
+}
+
+export default class LoadingDots extends Component<LoadingDotsProps, LoadingDotsState> {
+    static defaultProps: LoadingDotsProps = {
+        interval: 300,
+        dots: 3
+    }
+
+    private interval?: number;
+
+    constructor(props: LoadingDotsProps) {
+        super(props);
+        this.state = { frame: 1 };
+    }
+
+    componentDidMount(): void {
+        this.interval = window.setInterval(() => {
+            this.setState(prev => ({
+                frame: prev.frame + 1
+            }));
+        }, this.props.interval);
+    }
+
+    componentWillUnmount() {
+        window.clearInterval(this.interval);
+    }
+
+    render() {
+        let size = this.state.frame % (this.props.dots + 1)
+        const text = (new Array(size)).fill(".").join("");
+        return (
+            <span {...this.props}>{text}&nbsp;</span>
+        )
+    }
+}
